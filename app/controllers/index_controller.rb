7bc2b15c8ca2fd32a5372a56cdc994fb48c2342a
mzkiskw <srminmin1007@gmail.com>
2022-10-04 21:40:54 +0900

    Add show action

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,4 +2,8 @@ class IndexController < ApplicationController
   def index_action
     @tasks = Task.all
   end
+
+  def show_action
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "index#index_action"
+
+  get "/", to: "index#index_action"
+  get "/task", to: "index#index_action"
+  get "/task/:id", to: "index#show_action"
 end
